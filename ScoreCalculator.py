8fb460dfeacf92bb44594d8423d00f10e51d1e0f
Elshad19 <87479123+Elshad19@users.noreply.github.com>
2023-02-04 21:14:55 +0400

    Update ScoreCalculator.py
    The new version adds the option to redo the process without the need of terminating the program.

--- ScoreCalculator.py
+++ ScoreCalculator.py
@@ -1,75 +1,78 @@
-#V1
+import time
 
-print("This small project will help you to easily calculate total scores of students. \nPlease, write KSQ1 score.")
-ksq1 = input()
-print("KSQ1 score is " + ksq1)
-ksq1 = int(ksq1)
+#What is new(V2): slight delay between calculations, option to redo the proccess are added.
 
-print()
+def tc():
+    print("This small project will help you to easily calculate total scores of students. \nPlease, write sub summative (SS) 1 score.")
+    ksq1 = input()
+    print("SS1 score is " + ksq1)
+    ksq1 = int(ksq1)
+ 
+    print()
 
-print("Please, write KSQ2 score.")
-ksq2 = input()
-print("KSQ2 score is " + ksq2)
-ksq2 = int(ksq2)
+    print("Please, write SS2 score.")
+    ksq2 = input()
+    print("SS2 score is " + ksq2)
+    ksq2 = int(ksq2)
 
-print()
+    print()
 
-print("Please, write KSQ3 score.")
-ksq3 = input()
-print("KSQ3 score is " + ksq3)
-ksq3 = int(ksq3)
+    print("Please, write SS3 score.")
+    ksq3 = input()
+    print("SS3 score is " + ksq3)
+    ksq3 = int(ksq3)
 
-print()
+    print()
 
-print("Please, write BSQ score.")
-bsq = input()
-print("BSQ score is " + bsq)
-bsq = int(bsq)
+    print("Please, write big summative (BS) score.")
+    bsq = input()
+    print("BS score is " + bsq)
+    bsq = int(bsq)
 
-print()
+    print("\nCalculating...")
 
-sumKSQ = (ksq1 + ksq2 + ksq3) / 3
-beforerounding = sumKSQ
-beforerounding = str(beforerounding)
-print("Before rounding numerical average,  initial score is " + beforerounding + ".")
+    print()
 
-print ()
+    time.sleep(1.2)
 
-sumKSQ = round(sumKSQ)
-sumKSQ = str(sumKSQ)
-print("First, we find numerical average of KSQ scores. They are added first and then divided by three. \nRounded up numerical average is " + sumKSQ + ".")
-sumKSQ = int(sumKSQ)
+    sumKSQ = (ksq1 + ksq2 + ksq3) / 3
+    sumKSQ = str(sumKSQ)
+    print("First, we find numerical average of small summative scores. They are added first and then divided by three. \nNumerical average is " + sumKSQ + ".")
+    sumKSQ = float(sumKSQ)
 
-print()
+    time.sleep(0.5)
+    print()
 
-sumKSQ = sumKSQ * 40 / 100
-beforerounding2 = sumKSQ
-beforerounding2 = str(beforerounding2)
-print("Before rounding 40% value of numerical average, initial score is " + beforerounding2 + ".")
+    sumKSQ = sumKSQ * 0.4
+    sumKSQ = str(sumKSQ)
+    print("Then, we find 40% of numerical average. \nIt is " + sumKSQ + ".")
+    sumKSQ = float(sumKSQ)
 
-print ()
+    time.sleep(0.5)
+    print()
 
-sumKSQ = round(sumKSQ)
-sumKSQ = str(sumKSQ)
-print("Then, we find 40% of numerical average. \nRounded up value is " + sumKSQ + ".")
-sumKSQ = int(sumKSQ)
+    sumBSQ = bsq * 0.6
+    sumBSQ = str(sumBSQ) 
+    print("Third, we find 60% value of big summative score. \nIt is " + sumBSQ + ".")
+    sumBSQ = float(sumBSQ)
 
-print()
+    time.sleep(0.5)
+    print()
 
-sumBSQ = bsq * 60 / 100
-beforerounding3 = sumBSQ
-beforerounding3 = str(beforerounding3)
-print("Before rounding 60% value of BSQ score, initial score is " + beforerounding3 + ".")
-
-print()
-
-sumBSQ = round(sumBSQ)
-sumBSQ = str(sumBSQ)
-print("Third, we find 60% value of BSQ score. \nRounded up value is " + sumBSQ + ".")
-sumBSQ = int(sumBSQ)
-
-print()
-
-totalScore = (sumKSQ + sumBSQ)
-totalScore = str(totalScore)
-print("At last, we combine two values by adding. \nTotal score is " + totalScore + ".")
+    totalScore = (sumKSQ + sumBSQ)
+    totalScore = str(totalScore)
+    print("At last, we combine two values by adding. \nTotal score is " + totalScore + ".")
+ 
+    print("\nWould you like to try again? (1 = Yes or 0 = no)")
+    
+    def userInput():
+        userAnswer = input()  
+        if userAnswer == '1':
+            tc()
+        if userAnswer == '0':
+            print("See you next time!")
+        else :
+            print("Please write either 1 or 0")
+            userInput()
+    userInput()
+tc()
